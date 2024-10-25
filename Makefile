bc69240bf1053054e07d4ef7d06114f071734d86
Yiwu <wuyi96563@gmail.com>
2024-10-25 13:08:43 +0800

    sse done

--- Makefile
+++ Makefile
@@ -1,25 +1,44 @@
-.PHONY: clean all
+.PHONY: clean all release debug
 
-CFLAGS = -Wall -g
+CXX = g++
+CFLAGS = -Wall
+RELEASE_FLAGS = -O2
+DEBUG_FLAGS = -g
+
 targets = filelines filelines_gen testBlocked filelines_avx
 objects = filelines.o find_most_freq.o filelines_baseline.o
 
-all: $(targets)
-	echo "all done"
+all: release
+
+release: CFLAGS += $(RELEASE_FLAGS)
+release: $(targets)
+
+debug: CFLAGS += $(DEBUG_FLAGS)
+debug: $(targets)
+
 filelines: $(objects)
-	g++ $(CFLAGS) $(objects) -o $@
+	$(CXX) $(CFLAGS) $(objects) -o $@
+
 filelines_gen: filelines_gen.o find_most_freq.o
-	g++ $(CFLAGS) $^ -o $@
+	$(CXX) $(CFLAGS) $^ -o $@
+
 testBlocked: testBlocked.o find_most_freq.o 
-	g++ $(CFLAGS) $^ -o $@
+	$(CXX) $(CFLAGS) $^ -o $@
+
 filelines_avx: filelines_avx.o find_most_freq.o
-	g++ $(CFLAGS) $^ -o $@
+	$(CXX) $(CFLAGS) $^ -o $@
+
 %.o: %.cpp %.h
-	g++ $(CFLAGS) -c $< -o $@
+	$(CXX) $(CFLAGS) -c $< -o $@
+
 filelines_gen.o: filelines_gen.cpp find_most_freq.h
-	g++ $(CFLAGS) -c $< -o $@
+	$(CXX) $(CFLAGS) -c $< -o $@
+
 testBlocked.o: testBlocked.cpp find_most_freq.h TestResult.h
+	$(CXX) $(CFLAGS) -c $< -o $@
+
 filelines_avx.o: filelines_avx.cpp find_most_freq.h
-	g++ $(CFLAGS) -c $< -o $@
+	$(CXX) $(CFLAGS) -c $< -o $@
+
 clean:
-	rm -f $(objects) filelines_gen.o testBlocked.o $(targets)
+	rm -f $(objects) filelines_gen.o testBlocked.o filelines_avx.o $(targets)
--- filelines_avx.cpp
+++ filelines_avx.cpp
@@ -13,65 +13,74 @@ using namespace std;
 void filelines_avx(const char *filepath, uint32_t *total_line_num, uint32_t *line_num, const int blockSize = 64)
 {
     int handle;
-    const int BYTE_SIZE = 16; // 一个向量有16个字节
+    const int CHAR_SIZE = 16; // 一个向量有16个字节
     if ((handle = open(filepath, O_RDONLY)) < 0)
         return;
     char *bp = nullptr;
-    int err = posix_memalign((void **)&bp, BYTE_SIZE * sizeof(char), blockSize);
+    int err = posix_memalign((void **)&bp, CHAR_SIZE * sizeof(char), blockSize);
     if (err)
     {
         close(handle);
         return;
     }
     int cur_len = 0;
-
-    const __m128i endline = _mm_set1_epi8('\n');
+    char *temp = bp;                             // 向量指针，指向检查的向量的起始位置
+    const __m128i endline = _mm_set1_epi8('\n'); // 换行符向量，用于比较
     while (1)
     {
         int bytes_read = read(handle, bp, blockSize);
+
         if (bytes_read <= 0)
             break;
-        for (int i = 0; i < bytes_read; i += BYTE_SIZE)
+
+        // 遍历块内的char向量
+        for (temp = bp; temp < bp + bytes_read; temp += CHAR_SIZE)
         {
-            const __m128i s = _mm_load_si128((const __m128i *)(bp));
-            char *temp = bp;
-            __m128i x = _mm_cmpeq_epi8(s, endline);
-            unsigned short r = (unsigned short)(_mm_movemask_epi8(x));
+            const __m128i s = _mm_loadu_si128((const __m128i *)(temp));
+
+            __m128i x = _mm_cmpeq_epi8(s, endline);                    // 检查块内向量是否含有换行符，如果有那么对应的字节被设置为11111111
+            unsigned short r = (unsigned short)(_mm_movemask_epi8(x)); // 提取x中每一个字节的最高位，也就是向量内为换行符的字节的bitmap
+
+            // 向量内没有换行符
             if (r == 0)
             {
-                cur_len += BYTE_SIZE;
+                cur_len += CHAR_SIZE; // 记录上这一行的字符数，累加到下一向量的统计
                 continue;
             }
-            unsigned long vec_offset = 0; // 记录向量内的偏移量
+            unsigned long vec_offset = 0; // 记录换行符在向量内的偏移量
+
+            // 检查向量内的换行符
             while (true)
             {
-                int ffs = __builtin_ffs(r); // 记录掩码中第一个1的偏移量, 也是新一行的字符数(如果这一行全部都在向量内)
+                int ffs = __builtin_ffs(r); // 查找向量内第一个换行符在bitmap中的索引，从1开始
+
+                // 已经统计完向量内所有的换行符
                 if (ffs == 0)
                 {
-                    cur_len += BYTE_SIZE - vec_offset - 1; // 加上向量内剩余的字符数
-                    break;
+                    cur_len += CHAR_SIZE - vec_offset; // 加上向量内剩余的字符数
+                    break;                             // 退出，检查下一个向量
                 }
-                vec_offset += ffs - 1;
-                cur_len += ffs;
+                vec_offset += ffs;   // 下一行字符在向量内的起始位置
+                cur_len += ffs - 1;  // 记录这一行的字符数
                 (*total_line_num)++; // 行数增加
                 if (cur_len >= MAX_LEN)
                     line_num[MAX_LEN - 1]++; // 如果这一行长度超过1024个字符
                 else
                     line_num[cur_len]++; // 对应长度的行的个数增加
-                r >>= ffs;
-                cur_len = 0;
+                r >>= ffs;               // 消除bitmap中已经检索到的换行符
+                cur_len = 0;             // 重置行字符数，统计下一行
             }
         }
     }
     close(handle);
 }
-const char testFile[19] = "test/testDebug.txt";
+const char testFile[19] = "test/test.txt";
 uint32_t line_num[MAX_LEN];
 uint32_t total_line_num;
 
 int main(int argc, char const *argv[])
 {
-    const int blockSize = 32;
+    const int blockSize = 256 * 1024;
     uint32_t most_freq_len, most_freq_len_linenum;
     total_line_num = 0;
     auto start = high_resolution_clock::now();
